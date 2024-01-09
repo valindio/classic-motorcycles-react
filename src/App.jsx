11f6a7fc74c9cde9d4269227c758d556104a1b4d
Dio <117795110+valindio@users.noreply.github.com>
2024-01-09 23:48:47 +0700

    complete about section

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 import Header from "./components/Header/Header";
 import Section from "./components/Section/Section";
 import Banner from "./components/Banner/Banner";
+import About from "./components/About/About";
 
 function App() {
   return (
     <>
       <Header />
       <Section forSection={"banner__container"}>
-        <Banner/>
+        <Banner />
+      </Section>
+      <Section forSection={"about__container"}>
+        <About />
       </Section>
     </>
   );
---
+++ src/components/About/About.jsx
@@ -0,0 +1,21 @@
+import "./About.css";
+
+const About = () => {
+  return (
+    <>
+      <div className="section__header">About Us</div>
+      <p className="section__description">
+        Welcome to CLASSIC MOTORCYCLES, where passion meets timeless elegance on
+        two wheels. Our platform is a celebration of the enduring allure of
+        vintage motorcycles, capturing the essence of an era when craftsmanship
+        and style were paramount. Immerse yourself in a world of iconic rides,
+        where every engine tells a story, and each design reflects the artistry
+        of bygone days. Whether you&apos;re a seasoned enthusiast or a newcomer
+        to the world of classic bikes, join us in reviving the spirit of the
+        open road and embracing the unmatched charm of these legendary machines.
+      </p>
+    </>
+  );
+};
+
+export default About;
